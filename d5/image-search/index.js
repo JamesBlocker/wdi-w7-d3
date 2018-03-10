bcd0636d1f2388bbad20da426472b90662932e18
Byron <jamesblocker81@gmail.com>
2018-03-09 16:10:48 -0800

    gitignore and dotenv

--- d5/image-search/index.js
+++ d5/image-search/index.js
@@ -8,6 +8,45 @@ const PORT = 3000
 
 // mtrdOZaC06osxKRC1GTqvOyXtdnvdIrH - giphy api key (don't put api keys in code!!!)
 
+app.get("/search/:term", (req, res) => {
+    const options = {
+        method: 'get',
+        url: `https://api.giphy.com/v1/gifs/search?api_key=mtrdOZaC06osxKRC1GTqvOyXtdnvdIrH&q=${req.params.term}&limit=24&offset=0&rating=G&lang=en`
+    }
+
+    httpClient(options).then((apiResponse) => {
+        var imgStr = ''
+        var images = apiResponse.data.data
+
+        images.forEach((image) => {
+            const currentImageURL = image.images.fixed_height.url
+            imgStr += `<img src="${currentImageURL}" />`
+        })
+
+        res.send(imgStr)
+    })
+})
+
+app.get("/stickers/:term", (req, res) => {
+    const options = {
+        methos: 'get',
+        url: `https://api.giphy.com/v1/stickers/search?api_key=mtrdOZaC06osxKRC1GTqvOyXtdnvdIrH&q=${req.params.term}&limit=25&offset=0&rating=G&lang=en`
+    }
+
+    httpClient(options).then((apiResponse) => {
+        var stickerStr = ''
+        var stickers = apiResponse.data.data
+        
+        stickers.forEach((sticker) => {
+            console.log(sticker)
+            const currentStickerURL = sticker.images.fixed_height.url
+            stickerStr += `<img src="${currentStickerURL}" />`
+        })
+
+        res.send(stickerStr)
+    })
+})
+
 app.get('/random/:tag', (req, res) => {
     // where we send the request
     const options = { 
